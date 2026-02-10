8176975e5c1729c772829d2234bf089a423e983c
mohamadreza <mohamadrezabjr@gmail.com>
2026-02-10 23:14:32 +0330

    delete vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 
-import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { AuthProvider } from "@/lib/auth-context"
 import { ThemeProvider } from "@/lib/theme-context"
@@ -47,7 +46,6 @@ export default function RootLayout({
         <ThemeProvider>
           <AuthProvider>{children}</AuthProvider>
         </ThemeProvider>
-        <Analytics />
       </body>
     </html>
   )
